dd7aa1f2955f99cdfa391bbebd7581e37b0bca3a
Madhu Kumar D S <madhudskumar@live.in>
2016-02-10 19:31:54 +0530

    Main page

--- public/app/reg/bRegCtrl.js
+++ public/app/reg/bRegCtrl.js
@@ -1,15 +1,7 @@
 bplApp.controller('bRegCtrl',
     function ($scope, $window) {
-        $scope.logIn = function () {
-            var printContents = document.getElementById('content').innerHTML;
-            var originalContents = document.body.innerHTML;
-            document.body.innerHTML = printContents;
-            window.print();
-            document.body.innerHTML = originalContents;
-        };
-
-        $scope.cancelForm = function () {
-            $window.history.back();
-        };
+        $scope.contacts = [
+            {name:''}
+        ]
     }
 );
